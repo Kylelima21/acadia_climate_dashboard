2c1ee46fedb726ebb8249819331495a8c47d5357
Kyle Lima <klima@schoodicinstitute.org>
2025-01-24 14:35:11 -0500

    fixing image reading issue

---
+++ global.R
@@ -0,0 +1,40 @@
+#global 
+
+#### R shiny dashboard displaying climate data from NOAA (nClimGrid) and McFarland Hill station in Acadia NP ####
+
+#---------------------------------------------#
+####        Load Required Packages         ####
+#---------------------------------------------#
+
+library(shiny)
+library(shinydashboard)
+library(fresh)
+library(ggplot2)
+library(readr)
+library(plotly)
+library(tidyverse)
+library(dplyr)
+library(leaflet)
+
+#-----------------------#
+####    Read Data    ####
+#-----------------------#
+
+shiny.merged.temp <- read.csv("data/processed_data/shiny_merged_temp.csv")
+
+shiny.merged.precip <- read.csv("data/processed_data/shiny_merged_precip.csv")
+
+shiny.merged.anom <- read.csv("data/processed_data/shiny_merged_anom.csv")
+
+shiny.merged.precip.anom <- read.csv("data/processed_data/shiny_merged_precip_anom.csv")
+
+shiny.monthly.records <- read.csv("data/processed_data/shiny_monthly_records.csv")
+
+shiny.monthly.precip.records <- read.csv("data/processed_data/shiny_monthly_precip_records.csv")
+
+shiny.daily.temp.records <- read.csv("data/processed_data/shiny_daily_temp_records.csv")
+
+temperature.data.merged <- read.csv("data/processed_data/temperature_data_merged.csv")
+
+precipitation.data.merged <- read.csv("data/processed_data/precipitation_data_merged.csv")
+
